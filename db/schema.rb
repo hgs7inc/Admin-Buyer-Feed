c3288831ff8d949e7e7a1467c406d0764b82a4a4
mss7inc <33023828+mss7inc@users.noreply.github.com>
2017-12-29 20:39:39 +0530

    Add files via upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171228111528) do
+ActiveRecord::Schema.define(version: 20171229140753) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,55 @@ ActiveRecord::Schema.define(version: 20171228111528) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["admin_user_id"], name: "index_articles_on_admin_user_id"
+  end
+
+  create_table "dailyfeatureds", force: :cascade do |t|
+    t.string "headline"
+    t.text "description"
+    t.integer "material_id"
+    t.integer "supplier_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["material_id"], name: "index_dailyfeatureds_on_material_id"
+    t.index ["supplier_id"], name: "index_dailyfeatureds_on_supplier_id"
+  end
+
+  create_table "feeds", force: :cascade do |t|
+    t.integer "card_id"
+    t.string "card_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["card_id", "card_type"], name: "index_feeds_on_card_id_and_card_type"
+  end
+
+  create_table "materials", force: :cascade do |t|
+    t.integer "supplier_id"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_materials_on_product_id"
+    t.index ["supplier_id"], name: "index_materials_on_supplier_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_suppliers_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
