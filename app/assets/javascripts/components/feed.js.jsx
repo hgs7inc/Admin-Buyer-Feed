c26c21ac50f271f9cd5f9785ed9fc4ec0372fba3
Mani Sankar Nambaru <manisankar.nambaru@users.noreply.github.com>
2017-12-31 01:00:09 +0530

    Feed_react_buyer
    React feed components

---
+++ app/assets/javascripts/components/feed.js.jsx
@@ -0,0 +1,17 @@
+var SideNav = createReactClass({
+
+  render: function() {
+    return (
+    <div>harsha place ur feed sidenav here</div>
+    );
+  }
+});
+
+var Feed = createReactClass({
+
+  render: function() {
+    return (
+    <div>harsha place ur feed component here</div>
+    );
+  }
+});
